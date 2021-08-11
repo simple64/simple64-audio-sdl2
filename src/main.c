0dc775717458f61074d6a8b74163f8d5ac54372a
Logan <loganmc10@users.noreply.github.com>
2021-08-11 14:18:31 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -90,7 +90,7 @@ void InitAudio()
     dev = SDL_OpenAudioDevice(dev_name, 0, desired, obtained, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE);
     free(desired);
     hardware_spec = obtained;
-    VolSDL = SDL_MIX_MAXVOLUME
+    VolSDL = SDL_MIX_MAXVOLUME;
     SDL_PauseAudioDevice(dev, 0);
     paused = 0;
     audio_stream = SDL_NewAudioStream(AUDIO_S16SYS, 2, GameFreq, hardware_spec->format, 2, hardware_spec->freq);
